Geforce132/SecurityCraft

Initial feature commit

src/main/java/net/geforcemods/securitycraft/blocks/FrameBlock.java
@@ -1,12 +1,17 @@
 package net.geforcemods.securitycraft.blocks;
 
+import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.blockentities.FrameBlockEntity;
+import net.geforcemods.securitycraft.components.GlobalPositions;
+import net.geforcemods.securitycraft.util.LevelUtils;
 import net.geforcemods.securitycraft.util.PlayerUtils;
 import net.geforcemods.securitycraft.util.Utils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -18,6 +23,9 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
@@ -58,12 +66,48 @@ public class FrameBlock extends OwnableBlock implements SimpleWaterloggedBlock {
 
 	@Override
 	public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-		if (stack.getItem() == SCContent.CAMERA_MONITOR.get()) {
-			PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.FRAME.get().getDescriptionId()), Utils.localize("messages.securitycraft:frame.rightclick"), ChatFormatting.RED);
+		if (stack.getItem() == SCContent.CAMERA_MONITOR.get() && level.getBlockEntity(pos) instanceof FrameBlockEntity be && be.isOwnedBy(player)) {
+			if (stack.has(SCContent.BOUND_CAMERAS.get())) {
+				GlobalPositions cameras = stack.get(SCContent.BOUND_CAMERAS.get());
+
+				if (!cameras.isEmpty()) {
+					be.setCameraPositions(stack);
+					PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.FRAME.get().getDescriptionId()), Utils.localize("messages.securitycraft:frame.camerasSet"), ChatFormatting.GREEN);
+				}
+				else
+					PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.FRAME.get().getDescriptionId()), Utils.localize("messages.securitycraft:frame.emptyMonitor"), ChatFormatting.RED);
+			}
+
 			return ItemInteractionResult.SUCCESS;
 		}
+		else if (stack.getItem() == SCContent.KEY_PANEL.get()) //Conversion takes priority
+			return ItemInteractionResult.SKIP_DEFAULT_BLOCK_INTERACTION;
 
 		return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+	}
+
+	@Override
+	public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
+		if (level.getBlockEntity(pos) instanceof FrameBlockEntity be) {
+			boolean ownedByUser = be.isOwnedBy(player);
+
+			if (be.isDisabled())
+				player.displayClientMessage(Utils.localize("gui.securitycraft:scManual.disabled"), true);
+			else if (ownedByUser || be.isAllowed(player)) {
+				if (!be.getCameraPositions().isEmpty()) {
+					if (level.isClientSide) {
+						if (!be.isActivated() && be.getCurrentCamera() != null)
+							be.setCurrentCameraAndUpdate(be.getCurrentCamera());
+						else
+							ClientHandler.displayFrameScreen(be, !ownedByUser);
+					}
+
+					return InteractionResult.SUCCESS;
+				}
+			}
+		}
+
+		return InteractionResult.PASS;
 	}
 
 	@Override
@@ -97,5 +141,15 @@ public class FrameBlock extends OwnableBlock implements SimpleWaterloggedBlock {
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirror) {
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new FrameBlockEntity(pos, state);
+	}
+
+	@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+		return level.isClientSide ? createTickerHelper(type, SCContent.OWNABLE_BLOCK_ENTITY.get(), LevelUtils::blockEntityTicker) : null;
 	}
 }

src/main/java/net/geforcemods/securitycraft/entity/camera/CameraClientChunkCacheExtension.java
@@ -1,0 +1,68 @@
+package net.geforcemods.securitycraft.entity.camera;
+
+import java.util.function.Consumer;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import net.geforcemods.securitycraft.SecurityCraft;
+import net.geforcemods.securitycraft.compat.embeddium.EmbeddiumCompat;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.ChunkEvent;
+
+public class CameraClientChunkCacheExtension { //plagiarized from Immersive Portals
+	// the most chunk accesses are from the main thread,
+	// so we use two maps to reduce synchronization.
+	// the main thread accesses this map, without synchronization
+	private static final Long2ObjectOpenHashMap<LevelChunk> CHUNK_MAP = new Long2ObjectOpenHashMap<>();
+	// other threads read this map, with synchronization
+	//private static final Long2ObjectOpenHashMap<LevelChunk> chunkMapForOtherThreads =
+	//		new Long2ObjectOpenHashMap<>();
+	//TODO this impl is currently not threadsafe, should we make it? ImmPtl does it (see comments above)
+
+	public static void drop(ClientLevel level, ChunkPos chunkPos) {
+		LevelChunk chunk = CHUNK_MAP.get(chunkPos.toLong());
+
+		if (chunk != null) {
+			CHUNK_MAP.remove(chunkPos.toLong());
+			NeoForge.EVENT_BUS.post(new ChunkEvent.Unload(chunk));
+			level.unload(chunk);
+
+			if (SecurityCraft.IS_EMBEDDIUM_INSTALLED)
+				EmbeddiumCompat.onChunkStatusRemoved(level, chunkPos.x, chunkPos.z);
+		}
+	}
+
+	public static LevelChunk getChunk(int x, int z) {
+		return CHUNK_MAP.get(ChunkPos.asLong(x, z));
+	}
+
+	public static LevelChunk replaceWithPacketData(ClientLevel level, int x, int z, FriendlyByteBuf packetData, CompoundTag chunkTag, Consumer<ClientboundLevelChunkPacketData.BlockEntityTagOutput> tagOutput) {
+		long chunkPos = ChunkPos.asLong(x, z);
+		LevelChunk chunk = CHUNK_MAP.get(chunkPos);
+
+		if (chunk == null) {
+			chunk = new LevelChunk(level, new ChunkPos(x, z));
+			chunk.replaceWithPacketData(packetData, chunkTag, tagOutput);
+			CHUNK_MAP.put(chunkPos, chunk);
+		}
+		else
+			chunk.replaceWithPacketData(packetData, chunkTag, tagOutput);
+
+		level.onChunkLoaded(new ChunkPos(x, z));
+
+		if (SecurityCraft.IS_EMBEDDIUM_INSTALLED)
+			EmbeddiumCompat.onChunkStatusAdded(level, x, z);
+
+		NeoForge.EVENT_BUS.post(new ChunkEvent.Load(chunk, false));
+		return chunk;
+	}
+
+	public static void clear() {
+		CHUNK_MAP.clear();
+	}
+}

mermotec/SecurityCraftBallistixSupport

src/main/java/net/geforcemods/securitycraft/blocks/FrameBlock.java
@@ -1,12 +1,18 @@
 package net.geforcemods.securitycraft.blocks;
 
+import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.blockentities.FrameBlockEntity;
+import net.geforcemods.securitycraft.components.NamedPositions;
+import net.geforcemods.securitycraft.items.CameraMonitorItem;
+import net.geforcemods.securitycraft.util.LevelUtils;
 import net.geforcemods.securitycraft.util.PlayerUtils;
 import net.geforcemods.securitycraft.util.Utils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -18,6 +24,9 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
@@ -58,12 +67,48 @@ public class FrameBlock extends OwnableBlock implements SimpleWaterloggedBlock {
 
 	@Override
 	public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-		if (stack.getItem() == SCContent.CAMERA_MONITOR.get()) {
-			PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.FRAME.get().getDescriptionId()), Utils.localize("messages.securitycraft:frame.rightclick"), ChatFormatting.RED);
+		if (stack.getItem() == SCContent.CAMERA_MONITOR.get() && level.getBlockEntity(pos) instanceof FrameBlockEntity be && be.isOwnedBy(player)) {
+			if (stack.has(SCContent.BOUND_CAMERAS.get())) {
+				NamedPositions cameras = stack.getOrDefault(SCContent.BOUND_CAMERAS.get(), CameraMonitorItem.DEFAULT_NAMED_POSITIONS);
+
+				if (!cameras.isEmpty()) {
+					be.setCameraPositions(stack);
+					PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.FRAME.get().getDescriptionId()), Utils.localize("messages.securitycraft:frame.camerasSet"), ChatFormatting.GREEN);
+				}
+				else
+					PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.FRAME.get().getDescriptionId()), Utils.localize("messages.securitycraft:frame.emptyMonitor"), ChatFormatting.RED);
+			}
+
 			return ItemInteractionResult.SUCCESS;
 		}
+		else if (stack.getItem() == SCContent.KEY_PANEL.get()) //Conversion takes priority
+			return ItemInteractionResult.SKIP_DEFAULT_BLOCK_INTERACTION;
 
 		return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+	}
+
+	@Override
+	public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
+		if (level.getBlockEntity(pos) instanceof FrameBlockEntity be) {
+			boolean ownedByUser = be.isOwnedBy(player);
+
+			if (be.isDisabled())
+				player.displayClientMessage(Utils.localize("gui.securitycraft:scManual.disabled"), true);
+			else if (ownedByUser || be.isAllowed(player)) {
+				if (!be.getCameraPositions().isEmpty()) {
+					if (level.isClientSide) {
+						if (!be.isActivated() && be.getCurrentCamera() != null)
+							be.setCurrentCameraAndUpdate(be.getCurrentCamera());
+						else
+							ClientHandler.displayFrameScreen(be, !ownedByUser);
+					}
+
+					return InteractionResult.SUCCESS;
+				}
+			}
+		}
+
+		return InteractionResult.PASS;
 	}
 
 	@Override
@@ -97,5 +142,15 @@ public class FrameBlock extends OwnableBlock implements SimpleWaterloggedBlock {
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirror) {
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new FrameBlockEntity(pos, state);
+	}
+
+	@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+		return level.isClientSide ? createTickerHelper(type, SCContent.OWNABLE_BLOCK_ENTITY.get(), LevelUtils::blockEntityTicker) : null;
 	}
 }

src/main/java/net/geforcemods/securitycraft/entity/camera/CameraClientChunkCacheExtension.java
@@ -1,0 +1,62 @@
+package net.geforcemods.securitycraft.entity.camera;
+
+import java.util.function.Consumer;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import net.geforcemods.securitycraft.SecurityCraftClient;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.ChunkEvent;
+
+public class CameraClientChunkCacheExtension { //plagiarized from Immersive Portals
+	// the most chunk accesses are from the main thread,
+	// so we use two maps to reduce synchronization.
+	// the main thread accesses this map, without synchronization
+	private static final Long2ObjectOpenHashMap<LevelChunk> CHUNK_MAP = new Long2ObjectOpenHashMap<>();
+	// other threads read this map, with synchronization
+	//private static final Long2ObjectOpenHashMap<LevelChunk> chunkMapForOtherThreads =
+	//		new Long2ObjectOpenHashMap<>();
+	//TODO this impl is currently not threadsafe, should we make it? ImmPtl does it (see comments above)
+
+	public static void drop(ClientLevel level, ChunkPos chunkPos) {
+		LevelChunk chunk = CHUNK_MAP.get(chunkPos.toLong());
+
+		if (chunk != null) {
+			CHUNK_MAP.remove(chunkPos.toLong());
+			NeoForge.EVENT_BUS.post(new ChunkEvent.Unload(chunk));
+			level.unload(chunk);
+			SecurityCraftClient.INSTALLED_IUM_MOD.onChunkStatusRemoved(level, chunkPos.x, chunkPos.z);
+		}
+	}
+
+	public static LevelChunk getChunk(int x, int z) {
+		return CHUNK_MAP.get(ChunkPos.asLong(x, z));
+	}
+
+	public static LevelChunk replaceWithPacketData(ClientLevel level, int x, int z, FriendlyByteBuf packetData, CompoundTag chunkTag, Consumer<ClientboundLevelChunkPacketData.BlockEntityTagOutput> tagOutput) {
+		long chunkPos = ChunkPos.asLong(x, z);
+		LevelChunk chunk = CHUNK_MAP.get(chunkPos);
+
+		if (chunk == null) {
+			chunk = new LevelChunk(level, new ChunkPos(x, z));
+			chunk.replaceWithPacketData(packetData, chunkTag, tagOutput);
+			CHUNK_MAP.put(chunkPos, chunk);
+		}
+		else
+			chunk.replaceWithPacketData(packetData, chunkTag, tagOutput);
+
+		level.onChunkLoaded(new ChunkPos(x, z));
+		SecurityCraftClient.INSTALLED_IUM_MOD.onChunkStatusAdded(level, x, z);
+		NeoForge.EVENT_BUS.post(new ChunkEvent.Load(chunk, false));
+		return chunk;
+	}
+
+	public static void clear() {
+		CHUNK_MAP.clear();
+	}
+}